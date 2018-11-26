af19f8527c8c607ef9394d66f053464474577d85
Manik Sinha <Manik-Sinha@users.noreply.github.com>
2018-11-26 02:24:41 -0500

    First Version

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: python monaco_bot.py
---
+++ monaco_bot.py
@@ -0,0 +1,24 @@
+#Monaco Bot is a Discord bot that adds and removes the Playing Monaco role.
+#Copyright (C) 2018 Manik Sinha
+
+import os
+import discord
+#import asyncio
+TOKEN = os.environ['TOKEN']
+
+class MonacoBot(discord.Client):
+    #Possible improvement:
+    #Add code to add role Playing Monaco if it doesn't exist.
+    async def on_member_update(self, before, after):
+        if before.activity != after.activity:
+            if str(after.activity) == "Monaco":
+                role = discord.utils.get(after.guild.roles, name="Playing Monaco")
+                print(f"Adding role {role}")
+                await after.add_roles(role)
+            elif str(before.activity) == "Monaco":
+                role = discord.utils.get(before.guild.roles, name="Playing Monaco")
+                print(f"Removing role {role}")
+                await after.remove_roles(role)
+
+client = MonacoBot()
+client.run(TOKEN)
