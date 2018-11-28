4ba59abf1855fe0f61861137c0fc8c41085d05eb
Manik Sinha <Manik-Sinha@users.noreply.github.com>
2018-11-28 10:40:03 -0500

    Show emoji with command roulette

--- monaco_bot.py
+++ monaco_bot.py
@@ -8,16 +8,6 @@ from discord.ext import commands
 #import asyncio
 TOKEN = os.environ['TOKEN']
 
-THIEVES = (
-    'Lookout',
-    'Locksmith',
-    'Pickpocket',
-    'Cleaner',
-    'Gentleman',
-    'Redhead',
-    'Hacker',
-    'Mole')
-
 MonacoBot = commands.Bot(
     command_prefix = '!',
     activity = discord.Game("Logging in")
@@ -27,7 +17,7 @@ MonacoBot.remove_command("help")
 
 @MonacoBot.event
 async def on_ready():
-    await MonacoBot.change_presence(activity = discord.Game("@Monaco Bot help"))
+    await MonacoBot.change_presence(activity = discord.Game("For help type !help"))
 
 @MonacoBot.command()
 async def help(ctx):
@@ -42,11 +32,20 @@ async def help(ctx):
 @MonacoBot.command()
 async def roulette(ctx, blonde = ""):
     """Pick a random thief (use !roulette blonde to include blonde)"""
-    thieves = THIEVES
+    thieves = (
+        'Lookout',
+        'Locksmith',
+        'Pickpocket',
+        'Cleaner',
+        'Gentleman',
+        'Redhead',
+        'Hacker',
+        'Mole')
     if blonde.lower() == "blonde":
         thieves = thieves + ("Blonde",)
-    random_thief = random.choice(thieves)
-    await ctx.send(f":{random_thief}: {random_thief}")
+    thief = random.choice(thieves)
+    emoji = await commands.EmojiConverter().convert(ctx, thief)
+    await ctx.send(f"{emoji} {thief}")
 
 @MonacoBot.event
 async def on_member_update(before, after):
