89d0f80944afe89c640cfc4ecd6f979a64b57026
Manik Sinha <Manik-Sinha@users.noreply.github.com>
2018-11-29 22:15:35 -0500

    Add map command

--- monaco_bot.py
+++ monaco_bot.py
@@ -51,6 +51,147 @@ async def roulette(ctx, blonde = ""):
     emoji = await commands.EmojiConverter().convert(ctx, thief)
     await ctx.send(f"{emoji} {thief}")
 
+@MonacoBot.command(name='map')
+async def random_level(ctx, players = 0, campaign = "all"):
+    """Pick a random level."""
+    #Limit players to range 0, 4.
+    players = max(0, min(players, 4))
+    allchars = ("Redhead", "Hacker", "Gentleman", "Mole", "Locksmith", "Lookout", "Pickpocket", "Cleaner")
+    levels_dict = {
+        "The Prison Break":("Locksmith", "Lookout", "Pickpocket", "Cleaner"),
+        "Hijack at the Hairpin":("Locksmith", "Lookout", "Pickpocket", "Cleaner"),
+        "The Lebanese Embassy":("Mole","Locksmith", "Lookout", "Pickpocket", "Cleaner"),
+        "Banque Albert":("Mole","Locksmith", "Lookout", "Pickpocket", "Cleaner"),
+        "Manoir Moucharder":("Mole","Locksmith", "Lookout", "Pickpocket", "Cleaner"),
+        "Le Port de la Condamine":("Gentleman", "Mole", "Locksmith", "Lookout", "Pickpocket", "Cleaner"),
+        "Scorpion and the Frog":("Gentleman", "Mole", "Locksmith", "Lookout", "Pickpocket", "Cleaner"),
+        "Centre Hospitalier":("Gentleman", "Mole", "Locksmith", "Lookout", "Pickpocket", "Cleaner"),
+        "Securitech Corp.":("Hacker", "Gentleman", "Mole", "Locksmith", "Lookout", "Pickpocket", "Cleaner"),
+        "Musée Océanographique":("Hacker", "Gentleman", "Mole", "Locksmith", "Lookout", "Pickpocket", "Cleaner"),
+        "Discothèque Rouge":("Hacker", "Gentleman", "Mole", "Locksmith", "Lookout", "Pickpocket", "Cleaner"),
+        "Quartier Diamant":allchars,
+        "Galerie D'Art":allchars,
+        "Place du Palais":allchars,
+        "Casino de Monte Carlo":allchars,
+        "Hôtel de Monaco":("Mole", "Gentleman", "Hacker", "Redhead"),
+        "Épilogue":("Mole", "Gentleman", "Hacker", "Redhead"),
+        "The Sound of Violence":("Redhead", "Hacker", "Gentleman", "Locksmith", "Lookout", "Pickpocket", "Cleaner"),
+        "Foreign Affairs":allchars,
+        "Liquidity":allchars,
+        "Turf War":allchars,
+        "Contraband":allchars,
+        "Drowned Rats":allchars,
+        "False Teeth":allchars,
+        "Devil's Trick":allchars,
+        "Scent of a Rival":allchars,
+        "The Red Carpet":allchars,
+        "The Prestige":allchars,
+        "Pearls Before Swine":allchars,
+        "Royal Flush":allchars,
+        "One Last Job":allchars,
+        "Au Revoir":allchars,
+        "Identity":("Locksmith", "Lookout", "Pickpocket", "Cleaner"),
+        "Prologue: The Mole":("Mole", "Mole", "Mole", "Mole"),
+        "Prologue: The Lookout":("Lookout", "Lookout", "Lookout", "Lookout"),
+        "Prologue: The Locksmith":("Locksmith", "Locksmith", "Locksmith", "Locksmith"),
+        "Prologue: The Pickpocket":("Pickpocket", "Pickpocket", "Pickpocket", "Pickpocket"),
+        "Prologue: The Hacker":("Hacker", "Hacker", "Hacker", "Hacker"),
+        "Prologue: The Gentleman":("Gentleman", "Gentleman", "Gentleman", "Gentleman"),
+        "Prologue: The Blonde":("Blonde", "Blonde", "Blonde", "Blonde"),
+        "Epilogue: The Cleaner":("Cleaner", "Cleaner", "Cleaner", "Cleaner"),
+        "After the Fire":allchars,
+        "Cache":allchars,
+        "From Whence He Came":allchars,
+        "Scentimentality":allchars,
+        "Escape From Monaco":allchars,
+        "Lyon's Den":allchars,
+        "Hidden in Squalor":allchars,
+        "Paris, Forever":allchars,
+        "Passport Denied":("Mole", "Lookout", "Cleaner", "Redhead"),
+        "Corpse Flower":("Mole", "Lookout", "Cleaner", "Redhead"),
+        "Dark and Stormy":("Mole", "Lookout", "Cleaner", "Redhead")}
+    levels = [
+        "The Prison Break",
+        "Hijack at the Hairpin",
+        "The Lebanese Embassy",
+        "Banque Albert",
+        "Manoir Moucharder",
+        "Le Port de la Condamine",
+        "Scorpion and the Frog",
+        "Centre Hospitalier",
+        "Securitech Corp.",
+        "Musée Océanographique",
+        "Discothèque Rouge",
+        "Quartier Diamant",
+        "Galerie D'Art",
+        "Place du Palais",
+        "Casino de Monte Carlo",
+        "Hôtel de Monaco",
+        "Épilogue",
+        "The Sound of Violence",
+        "Foreign Affairs",
+        "Liquidity",
+        "Turf War",
+        "Contraband",
+        "Drowned Rats",
+        "False Teeth",
+        "Devil's Trick",
+        "Scent of a Rival",
+        "The Red Carpet",
+        "The Prestige",
+        "Pearls Before Swine",
+        "Royal Flush",
+        "One Last Job",
+        "Au Revoir",
+        "Identity",
+        "Prologue: The Mole",
+        "Prologue: The Lookout",
+        "Prologue: The Locksmith",
+        "Prologue: The Pickpocket",
+        "Prologue: The Hacker",
+        "Prologue: The Gentleman",
+        "Prologue: The Blonde",
+        "Epilogue: The Cleaner",
+        "After the Fire",
+        "Cache",
+        "From Whence He Came",
+        "Scentimentality",
+        "Escape From Monaco",
+        "Lyon's Den",
+        "Hidden in Squalor",
+        "Paris, Forever",
+        "Passport Denied",
+        "Corpse Flower",
+        "Dark and Stormy"]
+    level_ranges = {
+        "all":(0, 51),
+        "locksmith":(0, 16),
+        "pickpocket":(17, 32),
+        "origins":(33, 40),
+        "fin":(41, 48),
+        "pvp":(49, 51)}
+    #level = random.choice(list(levels_dict.keys()))
+    campaign = campaign.lower()
+    if campaign != "pvp":
+        #If campaign is not in ranges.keys() then we use "all" as the key.
+        level_range = level_ranges.get(campaign, level_ranges["all"])
+        level = random.choice(levels[level_range[0]:(level_range[1] + 1)])
+    elif campaign == "pvp":
+        pvp_levels = (
+            "Épilogue",
+            "Passport Denied",
+            "Corpse Flower",
+            "Dark and Stormy")
+        level = random.choice(pvp_levels)
+
+    thieves = random.sample(levels_dict[level], players)
+    get_emojis = commands.EmojiConverter().convert
+    emojis = [await get_emojis(ctx, thief) for thief in thieves]
+    message = "**" + str(level) + "**"
+    for i in range(len(thieves)):
+        message += "\n" + str(emojis[i]) + " " + str(thieves[i])
+    await ctx.send(message)
+
 @MonacoBot.event
 async def on_member_update(before, after):
     if before.activity != after.activity:
